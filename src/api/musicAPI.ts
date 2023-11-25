ef10a7e930237294f22e904bbb94c4cedbf41835
Oleg Markelov <o.markelov@g.nsu.ru>
2023-11-25 21:23:28 +0700

    Use cookie auth in musicAPI

--- src/api/musicAPI.ts
+++ src/api/musicAPI.ts
@@ -1,11 +1,9 @@
 import { MUSIC_ADDRESS } from './constants';
 
 
-export const fetchGetMusic = (path: string, signal: AbortSignal) =>
+export const fetchGetMusic = (path: string, signal?: AbortSignal) =>
     fetch(`${MUSIC_ADDRESS}/${path}`, {
-        'method': 'GET',
-        'headers': {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        },
+        method: 'GET',
+        credentials: 'include',
         signal,
     });

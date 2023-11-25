03784e49fab4da4574c79d512d66bafade15e974
Oleg Markelov <o.markelov@g.nsu.ru>
2023-11-26 03:35:08 +0700

    Encode uri in fetchGetMusic

--- src/api/musicAPI.ts
+++ src/api/musicAPI.ts
@@ -2,7 +2,7 @@ import { MUSIC_ADDRESS } from './constants';
 
 
 export const fetchGetMusic = (path: string, signal?: AbortSignal) =>
-    fetch(`${MUSIC_ADDRESS}/${path}`, {
+    fetch(encodeURI(`${MUSIC_ADDRESS}/${path}`), {
         method: 'GET',
         credentials: 'include',
         signal,

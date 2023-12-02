eeca69d0f48e47071494a3d7f756002f08a21881
Oleg Markelov <o.markelov@g.nsu.ru>
2023-12-02 23:26:55 +0700

    Create player layout

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,38 +1,101 @@
 <script setup lang='ts'>
 
-import { onMounted, ref } from 'vue';
+import { watchEffect, ref } from 'vue';
 
 import { SERVER_ADDRESS } from '../api/constants';
 import { useAbortController } from '../hooks/useAbortController';
 import { usePlayerStore } from '../stores/player';
-import { clamp } from '../util/numbers';
+import { clamp, convertToPercent } from '../util/numbers';
+import { trimFileExtension } from '../util/strings';
 
+import Music from './icons/Music.vue';
+import Next from './icons/Next.vue';
+import Previous from './icons/Previous.vue';
+import Refresh from './icons/Refresh.vue';
+import Repeat, { RepeatState } from './icons/Repeat.vue';
+import PlayOrPause from './icons/PlayOrPause.vue';
+import Shuffle, { ShuffleState } from './icons/Shuffle.vue';
+import Stop from './icons/Stop.vue';
+import RangeSlider from './RangeSlider.vue';
 import Rating from './Rating.vue';
 
+
+const initialTime = 0;
+const initialVolume = 0.5;
 
 const { signal } = useAbortController();
 const playerStore = usePlayerStore();
 const audioRef = ref<HTMLAudioElement>();
+const volumeRef = ref<number>(initialVolume);
+const isPlayingRef = ref<boolean>(false);
+const shuffleStateRef = ref<ShuffleState>(ShuffleState.NO);
+const repeatStateRef = ref<RepeatState>(RepeatState.NO);
 
-onMounted(() => {
+watchEffect(() => {
     if (audioRef.value) {
-        audioRef.value.volume = 0.1;
+        audioRef.value.volume = volumeRef.value / 2;
     }
 });
+
+const onTimeUpdated = (value: number) => {
+
+};
+
+const onVolumeUpdated = (value: number) => {
+    volumeRef.value = value;
+};
+
+const onStopClicked = () => {
+
+};
+
+const onReplayClicked = () => {
+
+};
+
+const onPlayOrPauseClicked = () => {
+    isPlayingRef.value = !isPlayingRef.value;
+};
+
+const onPreviousClicked = () => {
+
+};
+
+const onNextClicked = () => {
+
+};
+
+const onShuffleClicked = () => {
+    shuffleStateRef.value = (() => {
+        switch (shuffleStateRef.value) {
+            case ShuffleState.NO:
+                return ShuffleState.SHUFFLE_DIRECTORY;
+            case ShuffleState.SHUFFLE_DIRECTORY:
+                return ShuffleState.SHUFFLE_GLOBALLY;
+            case ShuffleState.SHUFFLE_GLOBALLY:
+                return ShuffleState.NO;
+        }
+    })();
+};
+
+const onRepeatClicked = () => {
+    repeatStateRef.value = (() => {
+        switch (repeatStateRef.value) {
+            case RepeatState.NO:
+                return RepeatState.CYCLE;
+            case RepeatState.CYCLE:
+                return RepeatState.SINGLE;
+            case RepeatState.SINGLE:
+                return RepeatState.NO;
+        }
+    })();
+};
 
 </script>
 
 <template>
-    <div class='fixed bottom-0 flex flex-col gap-2 items-center w-full'>
+    <div class='fixed bottom-0 w-full flex flex-col gap-2 items-center'>
         <p v-if='playerStore.isTrackBeingRated'>Loading...</p>
-        <p>Rating: {{ playerStore.track?.meta.comment?.rating }}</p>
-        <p>isTrackBeingRated: {{ playerStore.isTrackBeingRated }}</p>
-        <p>Track path: {{ playerStore.track?.path }}</p>
-        <Rating
-            :rating='clamp(playerStore.track?.meta.comment?.rating, 0, 5)'
-            :isActionable='!playerStore.isTrackBeingRated'
-            v-on:rate='rating => playerStore.rateTrack(rating, signal)'
-        />
         <audio
             class='w-full'
             ref='audioRef'
@@ -41,5 +104,75 @@ onMounted(() => {
             controls
             autoplay
         ></audio>
+        <div class='mt-10'><!-- REMOVE --></div>
+        <div class='p-4 pb-3 w-full flex flex-col gap-3 bg-neutral-900'>
+            <div class='flex gap-3 justify-center items-center'>
+                <Music
+                    color='#d4d4d4'
+                    :size='32'
+                />
+                <span>{{ trimFileExtension(playerStore.track?.meta.filename) }}</span>
+                <Rating
+                    :rating='clamp(playerStore.track?.meta.comment?.rating, 0, 5)'
+                    :isActionable='!playerStore.isTrackBeingRated'
+                    v-on:rate='rating => playerStore.rateTrack(rating, signal)'
+                />
+            </div>
+            <div class='w-full flex gap-5'>
+                <div class='flex gap-2 items-center'>
+                    <Stop @click='onStopClicked' />
+                    <Refresh @click='onReplayClicked' />
+                    <PlayOrPause
+                        :is-playing='isPlayingRef'
+                        @click='onPlayOrPauseClicked'
+                    />
+                    <Previous @click='onPreviousClicked' />
+                    <Next @click='onNextClicked' />
+                </div>
+                <div class='w-full flex gap-3 items-center'>
+                    <RangeSlider
+                        :initial-value='initialTime'
+                        v-on:update='onTimeUpdated'
+                        :step='.005'
+                    />
+                    <div class='mb-0.5 shrink-0'>
+                        <span>0:00</span>
+                        <span> / </span>
+                        <span>4:25</span>
+                    </div>
+                </div>
+                <div class='ml-auto flex gap-5 items-center'>
+                    <div class='flex gap-2 items-center'>
+                        <Shuffle
+                            :shuffle-state='shuffleStateRef'
+                            @click='onShuffleClicked'
+                        />
+                        <Repeat
+                            :repeat-state='repeatStateRef'
+                            @click='onRepeatClicked'
+                        />
+                    </div>
+                    <div class='flex gap-1 items-center'>
+                        <div class='w-20'>
+                            <RangeSlider
+                                :initial-value='initialVolume'
+                                v-on:update='onVolumeUpdated'
+                            />
+                        </div>
+                        <div>
+                            <span
+                                v-if='convertToPercent(volumeRef) < 10'
+                                class='invisible pointer-events-none'
+                            >10</span>
+                            <span
+                                v-else-if='convertToPercent(volumeRef) < 100'
+                                class='invisible pointer-events-none'
+                            >1</span>
+                            <span class='mb-0.5'>{{ convertToPercent(volumeRef) }}%</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
--- src/util/strings.ts
+++ src/util/strings.ts
@@ -1,3 +1,14 @@
 export const combineClassNames = (...classNames: (string | false | undefined)[]) =>
     classNames.filter(className => !!className).join(' ') as string;
 
+export const trimFileExtension = (str?: string) => {
+    if (!str) {
+        return str;
+    }
+
+    const lastDotIdx = str.lastIndexOf('.');
+
+    return lastDotIdx >= 0
+        ? str.slice(0, lastDotIdx)
+        : str;
+};
